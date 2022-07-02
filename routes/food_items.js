cbc6da9eca311732241c1a5ea72781ab53c16b03
Tyree Webber <webber.tyree@gmail.com>
2022-07-02 14:42:42 -0600

    update requirement to properly call db.js file

--- routes/food_items.js
+++ routes/food_items.js
@@ -7,7 +7,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('./db');
+const db = require('../db/db');
 
 module.exports = () => {
   router.get("/", (req, res) => {
@@ -24,4 +24,4 @@ module.exports = () => {
       });
   });
   return router;
-}
+};

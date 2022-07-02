6863da2a1436c0839df2fda7c89403b5785d7314
AVacAdok <avakuang8@gmail.com>
2022-07-02 23:07:57 +0000

    edit mainpage html/css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+$(document).ready(function () {
+
+  $('.select-btn').on('click', function () {
+    if ($('.options').is(":hidden")) {
+      $('.options').slideDown('slow');
+    } else {
+      $('.options').slideUp();
+    }
+  });
+
+  
+
+})

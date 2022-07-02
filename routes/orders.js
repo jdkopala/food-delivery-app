3337864666e923719d85416b19745c3566d655e0
Tyree Webber <webber.tyree@gmail.com>
2022-07-02 14:11:07 -0600

    add requires

--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db');
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     let query = `SELECT id FROM orders

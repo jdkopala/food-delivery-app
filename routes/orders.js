1b3570ccea7ed38a961768dcdad2683ee38eb57b
Tyree Webber <webber.tyree@gmail.com>
2022-07-02 14:05:22 -0600

    create orders route

---
+++ routes/orders.js
@@ -0,0 +1,21 @@
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT id FROM orders
+    ORDER BY id`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const orders = data.rows;
+        res.json({
+          orders
+        });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({
+            error: err.message
+          });
+      });
+  });
+};

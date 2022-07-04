45bc6b46d83e81a0e1decbfed31926070c24a084
Jesse Kopala <jdkopala@gmail.com>
2022-07-04 16:47:49 -0600

    Install Twilio and get basic code implemented

---
+++ public/scripts/send-sms.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19707158485',
+     to: process.env.MY_PHONE_NUMBER
+   })
+  .then(message => console.log(message.sid));

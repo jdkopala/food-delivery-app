5fc66b9427fcb105ddcc6e9892aace732580f182
Tyree Webber <webber.tyree@gmail.com>
2022-07-05 14:44:09 -0600

    implement thumbnails for food menu

--- db/seeds/02_food_items.sql
+++ db/seeds/02_food_items.sql
@@ -1,42 +1,42 @@
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Breakfast Plate', 'Two eggs, Two slices of bacon, two slices of toast and a pile of hashbrowns', 10, 999, 3, 'Breakfast', null);
+VALUES ('Breakfast Plate', 'Two eggs, Two slices of bacon, two slices of toast and a pile of hashbrowns', 10, 999, 3, 'Breakfast', 'https://i.imgur.com/IR2O2MS.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES('Breakfast Sandwich', 'Fried egg on a toasted bun with cheddar cheese and bacon. Served with hashbrowns', 10, 999, 4, 'Breakfast', null);
+VALUES('Breakfast Sandwich', 'Fried egg on a toasted bun with cheddar cheese and bacon. Served with hashbrowns', 10, 999, 4, 'Breakfast', 'https://i.imgur.com/Pj2XDxY.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Won Ton Soup', 'Hand-wrapped won tons with green onions', 10, 699, 4.5, 'Soups and Salads', null);
+VALUES ('Won Ton Soup', 'Hand-wrapped won tons with green onions', 10, 699, 4.5, 'Soups and Salads', 'https://i.imgur.com/7svXyji.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Caesar Salad', 'Crisp romaine lettuce, parmesan cheese and in-house made croutons with creamy caesar dressing', 8, 1199, 5, 'Soups and Salads', null);
+VALUES ('Caesar Salad', 'Crisp romaine lettuce, parmesan cheese and in-house made croutons with creamy caesar dressing', 8, 1199, 5, 'Soups and Salads', 'https://i.imgur.com/bcNweMa.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Traditional Greek Salad', 'A fresh mix of chopped cucumber, tomoatoes, green peppers, onions and olives. Topped with Feta Cheese and Greek Dressing. Served with garlic toast', 8, 1299, 5, 'Soups and Salads', null);
+VALUES ('Traditional Greek Salad', 'A fresh mix of chopped cucumber, tomoatoes, green peppers, onions and olives. Topped with Feta Cheese and Greek Dressing. Served with garlic toast', 8, 1299, 5, 'Soups and Salads', 'https://i.imgur.com/AKUYWc4.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Chicken Soup', 'Warm and hearty soup with chicken, mixed vegetables and egg noodles. Served with an in house baked bun', 5, 899, 5, 'Soups and Salads', null);
+VALUES ('Chicken Soup', 'Warm and hearty soup with chicken, mixed vegetables and egg noodles. Served with an in house baked bun', 5, 899, 5, 'Soups and Salads', 'https://i.imgur.com/KIZrEyW.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Spinach and Artichoke Dip', 'A dreamy, creamy mixture of spinach and artichoke, baked to perfection and topped with fresh tomato and green onion. Served with housemade potato chippers.', 10, 1599, 3, 'Appetizers', null);
+VALUES ('Spinach and Artichoke Dip', 'A dreamy, creamy mixture of spinach and artichoke, baked to perfection and topped with fresh tomato and green onion. Served with housemade potato chippers.', 10, 1599, 3, 'Appetizers', 'https://i.imgur.com/vrc5iyh.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Tater Tots', 'Crispy on the outside and soft in the middle. Served with sriracha mayonnaise.', 12, 899, 4, 'Appetizers', null);
+VALUES ('Tater Tots', 'Crispy on the outside and soft in the middle. Served with sriracha mayonnaise.', 12, 899, 4, 'Appetizers', 'https://i.imgur.com/YqkhAl0.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('French Fries', 'More delicious than ever, our piping hot, salted french fries are golden on the outside and fluffy on the inside.', 10, 899, 5, 'Appetizers', null);
+VALUES ('French Fries', 'More delicious than ever, our piping hot, salted french fries are golden on the outside and fluffy on the inside.', 10, 899, 5, 'Appetizers', 'https://i.imgur.com/FRP1l5W.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Deep-Friend Pickles', 'Panko breaded dill pickle spears, coated in parmesan cheese and served with ranch. Perfect for sharing!', 14, 1299, 5, 'Appetizers', null);
+VALUES ('Deep-Friend Pickles', 'Panko breaded dill pickle spears, coated in parmesan cheese and served with ranch. Perfect for sharing!', 14, 1299, 5, 'Appetizers', 'https://i.imgur.com/Pbn8bYP.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Poutine', 'Golden thick-cut french fries perfectly baked with house-made gravy and mozzarella.', 15, 1199, 5, 'Appetizers', null);
+VALUES ('Poutine', 'Golden thick-cut french fries perfectly baked with house-made gravy and mozzarella.', 15, 1199, 5, 'Appetizers', 'https://i.imgur.com/HzckHDR.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Steak Sandwich', 'Our fully seasoned seven-ounce New York strip steak is perfectly grilled to order and served on a toasted garlic Texas toast.', 15, 1599, 5, 'Entrees', null);
+VALUES ('Steak Sandwich', 'Our fully seasoned seven-ounce New York strip steak is perfectly grilled to order and served on a toasted garlic Texas toast.', 15, 1599, 5, 'Entrees', 'https://i.imgur.com/MuqHKzS.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Fish and Chips', 'Flaky golden halibut served on seasoned thick-cut french fries with a fresh lemon slice and a side of tartar sauce.', 15, 1599, 3, 'Entrees', null);
+VALUES ('Fish and Chips', 'Flaky golden halibut served on seasoned thick-cut french fries with a fresh lemon slice and a side of tartar sauce.', 15, 1599, 3, 'Entrees', 'https://i.imgur.com/RhGwGDz.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Butter Chicken', 'Our butter chicken is coated with a tasty blend of Indian spices and oven baked in a delicious creamy tomato curry sauce. Served with basmati rice and garlic naan.', 20, 1799, 4, 'Entrees', null);
+VALUES ('Butter Chicken', 'Our butter chicken is coated with a tasty blend of Indian spices and oven baked in a delicious creamy tomato curry sauce. Served with basmati rice and garlic naan.', 20, 1799, 4, 'Entrees', 'https://i.imgur.com/T7fFV1f.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Chicken Alfredo', 'Chicken noodles lavishly dressed in a creamy garlic Alfredo sauce, topped with mozzarella and fresh Parmesan cheese, baked to perfection. Served with garlic toast.', 15, 1699, 3.5, 'Entrees', null);
+VALUES ('Chicken Alfredo', 'Chicken noodles lavishly dressed in a creamy garlic Alfredo sauce, topped with mozzarella and fresh Parmesan cheese, baked to perfection. Served with garlic toast.', 15, 1699, 3.5, 'Entrees', 'https://i.imgur.com/w8VTVen.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Baked Lasagna', 'Fine lasagna noodles layered with hearty meat sauce, stringy mozzarella, and fresh Parmesan. Served with garlic toast.', 18, 1399, 5, 'Entrees', null);
+VALUES ('Baked Lasagna', 'Fine lasagna noodles layered with hearty meat sauce, stringy mozzarella, and fresh Parmesan. Served with garlic toast.', 18, 1399, 5, 'Entrees', 'https://i.imgur.com/AVG8uGk.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Pub Burger', 'Alberta beef patty served with pepper jack cheese, smokey bacon, fresh tomatoes, lettuce, pickles, sauteed mushrooms and onions, and a fiery chipotle aioli.', 16, 1699, 5, 'Handhelds', null);
+VALUES ('Pub Burger', 'Alberta beef patty served with pepper jack cheese, smokey bacon, fresh tomatoes, lettuce, pickles, sauteed mushrooms and onions, and a fiery chipotle aioli.', 16, 1699, 5, 'Handhelds', 'https://i.imgur.com/AVG8uGk.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Chicken Fingers and Fries', 'Breaded chicken breast fillets, with golden crispy fries and plum sauce. Served with carrot sticks.', 12, 1549, 4, 'Handhelds', null);
+VALUES ('Chicken Fingers and Fries', 'Breaded chicken breast fillets, with golden crispy fries and plum sauce. Served with carrot sticks.', 12, 1549, 4, 'Handhelds', 'https://i.imgur.com/g5hNdgH.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Double Up Grilled Cheese', 'A delicious cheese blend melted between three slices of Texas toast piled with crispy smokey bacon.', 10, 1199, 2, 'Handhelds', null);
+VALUES ('Double Up Grilled Cheese', 'A delicious cheese blend melted between three slices of Texas toast piled with crispy smokey bacon.', 10, 1199, 2, 'Handhelds', 'https://i.imgur.com/PObh4hJ.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Clubhouse', 'Lean turkey, shaved ham, and crispy bacon layered with cheddar cheese, lettuce, tomatoes, and mayo on three slices of toast.', 8, 1499, 4, 'Handhelds', null);
+VALUES ('Clubhouse', 'Lean turkey, shaved ham, and crispy bacon layered with cheddar cheese, lettuce, tomatoes, and mayo on three slices of toast.', 8, 1499, 4, 'Handhelds', 'https://i.imgur.com/TqkKfUI.png');
 INSERT INTO food_items (name, description, prep_time_minutes, price_cents, rating, menu_category, thumbnail_url)
-VALUES ('Maple Bison Burger', 'A thick and juicy bison patty topped with fresh lettuce, swiss cheese, a crispy onion ring, bacon marmalade, and chipotle aioli.', 12, 1799, 5, 'Handhelds', null);
+VALUES ('Maple Bison Burger', 'A thick and juicy bison patty topped with fresh lettuce, swiss cheese, a crispy onion ring, bacon marmalade, and chipotle aioli.', 12, 1799, 5, 'Handhelds', 'https://i.imgur.com/2wtUtB4.png');

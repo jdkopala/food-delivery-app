727510a6697a012e2a09ea2ac8a8cf57f72059d7
Jesse Kopala <jdkopala@gmail.com>
2022-07-07 10:31:25 -0600

    Cleanup unneccessary comments on server code

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+// Middleware lives here
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -35,25 +33,20 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
 const foodItemsRouter = require("./routes/food_items");
 const orderItemsRouter = require("./routes/order_items");
 const ordersRouter = require("./routes/orders");
 const smsRouter = require('./routes/sms');
 const smsResponseRouter = require('./routes/sms');
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
+// Here are the routes for our pages, mostly for db acccess
 app.use("/food_items", foodItemsRouter(db));
 app.use("/order_items", orderItemsRouter(db));
 app.use("/orders", ordersRouter(db));
 app.use("/sms", smsRouter(db));
 app.use("/sms-response", smsResponseRouter(db));
-// Note: mount other resources here, using the same pattern above
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
+// Home page(s)
 
 // When you land on the main page below
 // It should auto populate with the entire menu
